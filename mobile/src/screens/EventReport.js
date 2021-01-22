ac4669be2e670444a89db166a101cbf8ace735a4
Andrei Eremia <andrei.eremia@aquasoft.ro>
2021-01-22 23:01:41 +0200

    MOBILE FORM DONE

--- mobile/src/screens/EventReport.js
+++ mobile/src/screens/EventReport.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, ScrollView} from 'react-native';
 
 import EventReportForm from '../components/EventReportForm';
 
 const EventReport = () => {
   return (
-    <View style={styles.containerStyle}>
-      <EventReportForm headerText="Event Report" />
-    </View>
+    <ScrollView>
+      <View style={styles.containerStyle}>
+        <EventReportForm headerText="Event Report" />
+      </View>
+    </ScrollView>
   );
 };
 
